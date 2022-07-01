50bc7c389b5aa593947dff6bdc4a1eb757151b5a
Ahmed Valiaiev <ahmed.valiaiev@instant.co>
2022-07-01 20:15:06 +0200

    Create navbar and added fonts

---
+++ components/fonts.js
@@ -0,0 +1,9 @@
+import { Global } from '@emotion/react'
+
+const Fonts = () => (
+  <Global
+    styles={`@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&family=Montserrat:wght@300;400;700&display=swap');`}
+  ></Global>
+)
+
+export default Fonts
